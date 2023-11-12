bd57b101f6738493a9a494c52d32e81ebd7032a0
TheMonsters03 <joshuadurrant03@gmail.com>
2023-11-13 00:27:38 +0100

    Add fontawesome icons and update landing page layout

--- src/client/pages/LandingPage.tsx
+++ src/client/pages/LandingPage.tsx
@@ -1,30 +1,40 @@
 import { Layout, Col, Row, Button } from 'antd';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCopyright } from '@fortawesome/free-solid-svg-icons';
 import "./pages.css";
 
-const {Content} = Layout;
+const {Content, Footer} = Layout;
 
 function LandingPage() {
     return (
-        <Content className="content">
-            <Row>
-                <Col span={8}>
-                    <div>
-                        <span id="topic">
-                            <h5>FWE 2023</h5>
-                        </span>
-                        <h1 id="title">Open Data Map</h1>
-                    </div>
-                </Col>
-                <Col span={12}/>
-                <Col span={4}>
-                    <Button type="text" icon={<i className="fa-solid fa-bars"></i>}>HERE</Button>
-                    <Link to="/content">
-                        <Button>CONTENT</Button>
-                    </Link>
-                </Col>
-            </Row> 
-        </Content>
+        <Layout className="layout">
+            <Content className="content">
+                <Row>
+                    <Col span={8}>
+                        <div>
+                            <span id="topic">
+                                <h5>FWE 2023</h5>
+                            </span>
+                            <h1 id="title">Open Data Map</h1>
+                        </div>
+                    </Col>
+                    <Col span={12}/>
+                    <Col span={4}>
+                        <Button type="text" icon={<FontAwesomeIcon icon={faBars}/>}>HERE</Button>
+                        <Link to="/content">
+                            <Button>CONTENT</Button>
+                        </Link>
+                    </Col>
+                </Row> 
+            </Content>
+            <Footer className='footer'>
+                <span>
+                    <FontAwesomeIcon icon={faCopyright}/>
+                    <p>Elias Csuka, Joshua Durrant, Leander Hemmi, Cedric Koller, Mathias Schmid; 2023</p>
+                </span>
+            </Footer>
+        </Layout>
     );
 }
 
